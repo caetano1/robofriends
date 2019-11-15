27401fcf7c2752b6a88d616584aeda08e3222ed6
caetano <joaoluisfcaetano@gmail.com>
2019-11-15 19:54:03 -0300

    Finished manipulation regardin the robots filtering

--- src/App.js
+++ src/App.js
@@ -20,24 +20,25 @@ class App extends Component {
     };
     
     // arrow function makes it sure that the instance will be the App
+    // otherwise, it would be the input control
     onSearchChange = (e) => {
         //console.log(e.target.value);
 
         //in order to change the state
         this.setState ({ searchfield: e.target.value })
-
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-        });
-        //console.log(filteredRobots)
     };
 
     render () {
+
+        // this allows to pass the filteredRobots to define the array in the CardList
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        });
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={this.state.robots}/>
+                <CardList robots={filteredRobots}/>
             </div>
         );
     }
